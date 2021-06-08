b720e707c29dc005d239e4dd44b16dfbe22c2d06
virensuthar <virensuthar5879@gmail.com>
2021-06-08 23:19:09 +0530

    removed StripeCheckout commponent fron Cart

--- projfrontend/src/core/Cart.js
+++ projfrontend/src/core/Cart.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import '../styles.css';
+import { API } from '../backend';
 import Base from './Base';
 import Card from './Card';
 import { loadCart } from './helper/cartHelper';
-import StripeCheckout from './StripeCheckout';
-function Cart() {
+
+const Cart = () => {
 	const [products, setProducts] = useState([]);
 	const [reload, setReload] = useState(false);
 
@@ -16,26 +17,23 @@ function Cart() {
 		return (
 			<div>
 				<h2>This section is to load products</h2>
-				{products.map((product, index) => {
-					return (
-						<Card
-							key={index}
-							product={product}
-							removeFromCart={true}
-							addtoCart={false}
-							setReload={setReload}
-							reload={reload}
-						/>
-					);
-				})}
+				{products.map((product, index) => (
+					<Card
+						key={index}
+						product={product}
+						removeFromCart={true}
+						addtoCart={false}
+						setReload={setReload}
+						reload={reload}
+					/>
+				))}
 			</div>
 		);
 	};
-
 	const loadCheckout = () => {
 		return (
 			<div>
-				<h2>This section is for checkout</h2>
+				<h2>This section for checkout</h2>
 			</div>
 		);
 	};
@@ -44,11 +42,10 @@ function Cart() {
 		<Base title='Cart Page' description='Ready to checkout'>
 			<div className='row text-center'>
 				<div className='col-6'>{loadAllProducts()}</div>
-				<div className='col-6'>
-					<StripeCheckout products={products} setReload={setReload} />
-				</div>
+				<div className='col-6'>Payment Section</div>
 			</div>
 		</Base>
 	);
-}
+};
+
 export default Cart;
